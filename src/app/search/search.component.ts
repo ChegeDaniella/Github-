531643a08b986813e7a293922e9a97b118771904
ChegeDaniella <chegedaniella@gmail.com>
2020-04-10 14:40:59 +0300

    ensuring data is entering the form

---
+++ src/app/search/search.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../user'
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent implements OnInit {
+  inputUser= new User();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
